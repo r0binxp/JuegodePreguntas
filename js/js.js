3d335a1562b1bdb309fb27439d9e0bb0649e58e7
LuksFay <lukspicco@hotmail.com>
2015-02-07 20:03:15 -0300

    Agregue respuestas al JS
    eso

--- js/js.js
+++ js/js.js
@@ -3,8 +3,11 @@
 var Preguntas=["Quien es el jefe final (del primer final) del juego The binding of Isaac? \n Su Hermano = 0 \n Su Madre = 1  \n  Su Padre \n Su Amigo = 3",
 		"Codigo de la bomba del Counter Strike \n 7355608 = 0 \n 8750355 = 1 \n 7834446 = 2 \n 7355680 = 3",
 		"Cuantas evoluciones tiene Eevee actualmente \n Tres = 0 \n Nueve = 1 \n Ocho = 2 \n Cinco = 3",
-		"Que significa Weon (palabra chilena) \n Es un insulto = 0 \n  Amigo = 1 \n Weon = 2 \n Es una comida = 3",
+		"Que significa Weon (palabra chilena) \n Un insulto = 0 \n  Un cuadro = 1 \n Un numero = 2 \n Una comida = 3",
 		"Arte marcial Creada por Bruce Lee \n Tae Kwon Do = 0 \n Judo = 1 \n Jet Kune Do = 2 \n Karate Do = 3",
 		"Nombre del ratón emblematico de Disney \n Mickey = 0 \n Jerry = 1 \n Pikachu = 2 \n Donald = 3";
 
 		]
+// El que lee esto es puto
+
+var RespuestasC=[ "1", "0", "0", "2" , "2", "0" ] 
